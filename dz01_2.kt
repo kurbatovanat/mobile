8b84e3d64d4be784006605e3ee0a160f03667742
kurbatovanat <147327403+kurbatovanat@users.noreply.github.com>
2024-09-12 14:40:04 +0300

    Update

---
+++ dz01_2.kt
@@ -0,0 +1,9 @@
+fun main() {
+    const val myAge : Int = 57
+    var myAgeInTenYears : Int = myAge + 10
+    val daysInYear = 365.25
+    var daysPassed : Float = myAgeInTenYears * daysInYear
+    print("Мой возраст \(myAge) лет. Через 10 лет мне будет \(myAgeInTenYears) лет, с момента моего рождения пройдет \(daysPassed) дней.\n")
+}
+
+
